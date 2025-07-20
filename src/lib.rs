fa1dcd62db9a60a1a15ead22dc73326090594341
Rob Wells <rob@robjwells.com>
2025-07-20 09:38:50 +0100

    Crate is currently non-functional.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod parse;
 
 use std::error::Error;
 
-use parse::site_forecast_from_reader;
+// use parse::site_forecast_from_reader;
 pub use parse::{Coordinates, HourlyForecast, SiteForecast};
 
 pub struct ApiKey {
@@ -13,43 +13,9 @@ pub struct ApiKey {
 }
 
 pub fn fetch_hourly_forecasts(
-    key: &ApiKey,
-    latitude: f64,
-    longitude: f64,
+    _key: &ApiKey,
+    _latitude: f64,
+    _longitude: f64,
 ) -> Result<SiteForecast, Box<dyn Error>> {
-    let lat = format!("{latitude}");
-    let lon = format!("{longitude}");
-    let query_params = [
-        ("includeLocationName", "true"),
-        ("excludeParameterMetadata", "true"),
-        ("latitude", &lat),
-        ("longitude", &lon),
-    ];
-    let client = reqwest::blocking::Client::new()
-        .get("https://api-metoffice.apiconnect.ibmcloud.com/v0/forecasts/point/hourly")
-        .query(&query_params)
-        .header("X-IBM-Client-Id", &key.id)
-        .header("X-IBM-Client-Secret", &key.secret);
-
-    let response = client.send()?;
-    if cfg!(debug_assertions) {
-        let ratelimit_header = response.headers().get("x-ratelimit-remaining");
-        debug_assert!(ratelimit_header.is_some());
-        let remaining = ratelimit_header
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .split(',')
-            .nth(1)
-            .unwrap()
-            .replace(';', "");
-        eprintln!(
-            "DEBUG :: API :: Remaining Met Office API calls :: {}",
-            remaining
-        );
-    }
-
-    let body = response.text()?;
-    let parsed = site_forecast_from_reader(body.as_bytes()).unwrap();
-    Ok(parsed)
+    Err("The service this crate relied on is no longer provided.".into())
 }
